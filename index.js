328d42dc69e49278544c468c92e38d2fef711e4e
Mgilmore2020 <believe2@colostate.edu>
2020-11-06 12:10:21 -0700

    Deployment prep

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
+const cors = require("cors")
 
 const db = require("./dbConnect.js")
 const config = require("./config.js")
 const auth = require("./middleware/authenticate")
+
+//azurewebsites.net, colostate.edu
 const app = express();
 app.use(express.json())
-
+app.use(cors())
 
 
 app.post("/applications", auth, async (req,res)=>{
@@ -200,4 +203,5 @@ app.get("/workplaces/:pk", (req, res)=>{
         })
 })
 
-app.listen(5000,()=>{console.log("app is running on port 5000")});
+const PORT = process.env.PORT || 5000
+app.listen(PORT,()=>{console.log(`app is running on port ${PORT}`)});
